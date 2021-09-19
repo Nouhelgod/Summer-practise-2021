99a6425e07ec0e9e91e6367ab8b2a5af3f0245cd
Nouhelgod <36785937+Nouhelgod@users.noreply.github.com>
2021-09-20 00:45:31 +0600

    Задание 1.1

--- Src/main.py
+++ Src/main.py
@@ -4,16 +4,20 @@ import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 
 directory = os.path.dirname(__file__)
-pic = os.path.join(directory, 'pic1.jpg')
+
+PIC = os.path.join(directory, 'pic1.jpg')
+TEXT = u'Кто пёс?\nЯ пёс?\nА может ты пёс?'
 
 
-with cbook.get_sample_data(pic) as image_file:
+with cbook.get_sample_data(PIC) as image_file:
     image = plt.imread(image_file)
 
-fig, ax = plt.subplots()
+fig = plt.figure()
 
-ax.imshow(image)
-ax.text(300, 50, u'Кто пёс?\nЯ пёс?\nА может ты пёс?', size='x-large')
+ax_1 = fig.add_subplot(2, 3, 5)
+
+ax_1.imshow(image)
+ax_1.text(300, 50, TEXT, size='x-large')
 
 fig.set_figwidth(7)
 fig.set_figheight(7)
